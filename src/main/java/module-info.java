3e4596e79f55cad4c51988bb4d08c3cc4251b3d7
Claus Nagel <cnagel@virtualcitysystems.de>
2022-06-16 15:54:18 +0200

    fixed module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module org.xmlobjects.classindex {
 
     exports org.atteo.classindex;
     exports org.atteo.classindex.processor;
-    exports org.atteo.evo.classindex;
 
     provides javax.annotation.processing.Processor with org.atteo.classindex.processor.ClassIndexProcessor;
 }
